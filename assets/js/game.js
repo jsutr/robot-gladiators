9e531344965be285de69539f8d5e1c5c6ce7b1ec
justinsuter-qiKFlV <suter765@gmail.com>
2022-02-12 14:57:40 -0800

    initial player and enemy variables and player name prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! Java is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+console.log(playerName);
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
