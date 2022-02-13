28b26a81907648b70dcbf3768279059cb63291fa
justinsuter-qiKFlV <suter765@gmail.com>
2022-02-13 00:29:12 -0800

    first round implemented

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,7 +9,38 @@ var enemyAttack = 12;
 console.log(playerName);
 
 var fight = function() {
+    //Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
+
+    //Subtract value of playerAttack from enemyHealth
+    enemyHealth = enemyHealth - playerAttack
+
+    //Log resulting message to console to confirm proper execution
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + "health left.");
+    }
+
+    //Subtract enemyAttack from playerHealth
+    playerHealth = playerHealth - enemyAttack
+
+    //Log resulting message to console to confirm proper execution
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 };
 
 fight();
